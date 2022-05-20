20ab6a2bcb01e4ace73c52fce437e45b8eaa3703
Luis Mario Franco <lmfg06@gmail.com>
2022-05-20 17:56:42 -0500

    Upload documentation

--- src/Firebase/AdminToken.ts
+++ src/Firebase/AdminToken.ts
@@ -3,20 +3,23 @@ import admin from "./firebase";
 
 export const decodeToken = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(" ")[1];
-    console.log(token);
     try {
         if (token === undefined) {
             return res.status(401).json({ message: "Unauthorized" });
         } else {
             const decodeValue = await admin.auth().verifyIdToken(token!);
-            console.log(decodeValue);
             if (decodeValue != null || decodeValue != undefined) {
                 return next();
             }
             return res.json({ message: "Unauthorized" });
         }
     } catch (error) {
-        console.log(error);
-        return res.json({ message: "Internal Server Error" }).status(500);
+        if (error.code === "auth/id-token-expired") {
+            return res.json({ message: "Token expired" }).status(401);
+        } else if (error.code === "auth/argument-error") {
+            return res.json({ message: "Invalid token" }).status(401);
+        } else {
+            return res.json({ message: "Internal Server Error" }).status(500);
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,26 @@ import { connectDatabase } from "./services/database.service";
 import { countrieRouter } from "./Routes/countries.routes";
 import { decodeToken } from "./Firebase/AdminToken";
 
+import path from "path";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
+const swaggerSpec = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Backend Reto Final Etapa 1',
+            version: '1.0.0'
+        },
+        servers: [
+            {
+                url: 'http://localhost:8080/',
+            }
+        ]
+    },
+    apis: [`${path.join(__dirname, './Routes/*.js')}`]
+}
+
 const app = express();
 dotenv.config();
 
@@ -13,9 +33,8 @@ app.set("port", process.env.PORT || 3000);
 connectDatabase()
     .then(() => {
         app.use(morgan("dev"));
-        app.use(decodeToken);
+        app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(swaggerSpec)))
         app.use("/countries", countrieRouter)
-
         app.listen(app.get("port"), () => {
             console.log(`Server running on port ${app.get("port")}`);
         })
